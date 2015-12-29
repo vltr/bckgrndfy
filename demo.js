78bddfba16df0281ceb281a1b6a8cf8a39f0de39
Richard Gerd Kuesters <rkuesters@gmail.com>
2015-12-29 18:01:37 -0200

    oukey dokay, starting to rewrite it as vanilla js ... or something

--- demo.js
+++ demo.js
@@ -1,11 +1,9 @@
-goog.require('goog.dom')
-goog.require('wythe.lowpolybg')
-
 window.onload = function() {
-    var body = goog.dom.getElementsByTagNameAndClass('body')[0];
-    var canvas = wythe.lowpolybg.createLowPolyCanvas('HelloWorld', {width:600, height:300,cellSize:45});
-    canvas.style.left = '0px';
-    canvas.style.top = '0px';
-    canvas.style.position = 'absolute';
-    body.appendChild(canvas);
+    var demo = document.getElementById('demo');
+    var canvas = generatePolyCanvas('demo', {
+        width: 600,
+        height: 300,
+        cellSize: 45
+    });
+    demo.appendChild(canvas);
 }
